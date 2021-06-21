4174618d6b020acda0f5643cc1ceaf25902c8f68
Melinda Zhang <melinda_hongye@yahoo.com>
2021-06-21 12:53:30 +0100

    Add frontend bootstrap template and custom style

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from flask import Flask, flash, render_template, redirect, request, session, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 if os.path.exists("env.py"):
     import env
@@ -8,9 +10,16 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "hello world ... again!"
+def home():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
