e472c185e9570937b03880bc90ccf967b70b68d0
Melinda Zhang <melinda_hongye@yahoo.com>
2021-06-21 20:32:38 +0100

    Rename about.html to benefits.html and update content

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/benefits")
+def benefits():
+    return render_template("benefits.html")
 
 
 @app.route("/register")
