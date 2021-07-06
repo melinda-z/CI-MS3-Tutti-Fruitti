db2addfe1d164f3886abc366dfaa9d6e5779cba0
Melinda Zhang <melinda_hongye@yahoo.com>
2021-07-06 21:21:30 +0100

    Fix responsive issues

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
   $(".slider").slider();
   $("select").formSelect();
 });
-// Add ingredient functionality
+
+// Add ingredient input functionality
 var add = document.getElementById("add");
 var input = document.getElementById("new-ingredient");
 var ul = document.getElementById("ingredients-list");
@@ -20,8 +21,6 @@ function createInput() {
   newInput.setAttribute("minlength", "5");
   newInput.setAttribute("maxlength", "50");
   newInput.setAttribute("placeholder", "Enter Your Ingredient");
-  newInput.setAttribute("class", "col");
-  newInput.setAttribute("class", "s8");
   var deleteButton = document.createElement("button");
   var t = document.createTextNode("Delete");
   deleteButton.appendChild(t);
@@ -34,36 +33,10 @@ function createInput() {
   deleteButton.addEventListener("click", deleteInput);
 }
 
-// function createListElement() {
-//   var li = document.createElement("li");
-//   li.appendChild(document.createTextNode(input.value));
-//   ul.appendChild(li);
-//   input.value = "";
-//   var deleteButton = document.createElement("button");
-//   deleteButton.style.marginLeft = "60px";
-//   deleteButton.style.backgroundColor = "#81c784";
-//   deleteButton.style.borderRadius = "5px";
-//   deleteButton.className = "b";
-
-//   var buttonText = document.createTextNode("Delete");
-//   deleteButton.appendChild(buttonText);
-//   li.appendChild(deleteButton);
-//   function deleteItems() {
-//     li.remove();
-//   }
-//   deleteButton.addEventListener("click", deleteItems);
-// }
 function addListAfterClick() {
   if (inputLength() > 0) {
     createInput();
   }
 }
 
-function addListAfterKeypress(event) {
-  if (inputLength() > 0 && event.keyCode === 13) {
-    createInput();
-  }
-}
 add.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
